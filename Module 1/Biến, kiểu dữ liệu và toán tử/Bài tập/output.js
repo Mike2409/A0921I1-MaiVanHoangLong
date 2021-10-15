b728e78bc246bebcc7813ec2f6078e28aa637f95
Mike2409 <91193916+Mike2409@users.noreply.github.com>
2021-10-15 19:03:38 +0700

    Add files via upload

---
+++ Module 1/Biến, kiểu dữ liệu và toán tử/Bài tập/output.js
@@ -0,0 +1,14 @@
+let inputPhysics;
+let inputBiology;
+let inputMath;
+
+inputPhysics = prompt("Nhập điểm vật lý");
+inputBiology = prompt("Nhập điểm sinh học");
+inputMath = prompt("Nhập điểm môn toán")
+
+let inputPhysics = parseInt(inputPhysics);
+let inputBiology = parseInt(inputBiology);
+let inputMath = parseInt(inputMath)
+
+let area = width * height;
+document.write("The area is: " + area);
