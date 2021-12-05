1da1690f381e9b286e4cf995cb3e0b815c732980
Mike2409 <91193916+Mike2409@users.noreply.github.com>
2021-12-05 07:46:26 +0700

    Add files via upload

---
+++ Module 2/bai_tap/ConvertMoney.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ConvertMoney {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào tiền USD bạn muốn chuyển đổi: ");
+        double Usd = scanner.nextFloat();
+
+        double Vnd = Usd * 23000;
+
+        System.out.println("Tiền VND sẽ là: " + Vnd + " VND.");
+    }
+}
