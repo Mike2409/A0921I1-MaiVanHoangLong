e831999dc5b3b517f993fdda2af84e3aa36f455f
Mike2409 <91193916+Mike2409@users.noreply.github.com>
2021-12-17 11:30:46 +0700

    Add files via upload

---
+++ Module 2/ss6_inheritance/bai_tap/test.java
@@ -0,0 +1,9 @@
+package ss6_inheritance.bai_tap;
+
+public class test {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(2,"red",3);
+        System.out.println(cylinder);
+
+    }
+}
