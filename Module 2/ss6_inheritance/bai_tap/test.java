38a3b13321d2d3765f795c044a1d4f45516a6765
Mike2409 <91193916+Mike2409@users.noreply.github.com>
2021-12-18 21:20:20 +0700

    Delete Module 2/ss6_inheritance/bai_tap directory

--- Module 2/ss6_inheritance/bai_tap/test.java
+++
@@ -1,9 +0,0 @@
-package ss6_inheritance.bai_tap;
-
-public class test {
-    public static void main(String[] args) {
-        Cylinder cylinder = new Cylinder(2,"red",3);
-        System.out.println(cylinder);
-
-    }
-}
